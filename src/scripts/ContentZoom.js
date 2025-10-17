85165deb954ddab3a67447a80e135e83cf760a73
Christopher Canisius <cc@electronic-minds.de>
2025-10-17 18:58:28 +0200

    feat: Implement our shared base class

--- src/scripts/ContentZoom.js
+++ src/scripts/ContentZoom.js
@@ -1,21 +1,26 @@
-export class ContentZoom {
-    /**
-     * @param {HTMLElement} container
-     * @param {HTMLElement} overflowElement
-     * @param {string} buttonLabel
-     */
-    constructor(
-        container = document.querySelector('.content-zoom'),
-        overflowElement,
-        buttonLabel = 'Zoom'
-    ) {
-        if (typeof overflowElement === 'undefined') {
-            overflowElement = container;
-        }
+import { Base } from '@ryze-digital/js-utilities';
 
-        this.container = container;
-        this.overflowElement = overflowElement;
-        this.buttonLabel = buttonLabel;
+export class ContentZoom extends Base {
+    /**
+     * @param {object} options
+     * @param {HTMLElement} [options.container]
+     * @param {HTMLElement} [options.overflowElement]
+     * @param {string} [options.buttonLabel]
+     */
+    constructor(options) {
+        super({
+            container: document.querySelector('.content-zoom'),
+            overflowElement: null,
+            buttonLabel: 'Zoom',
+            classes: {
+                contendZoomed: 'zoom',
+                triggerButton: 'content-zoom-trigger'
+            }
+        }, options);
+
+        if (this.options.overflowElement === null) {
+            this.options.overflowElement = this.options.container;
+        }
 
         if (this.#isContentOverflowing) {
             this.#appendZoomButton();
@@ -26,24 +31,24 @@ export class ContentZoom {
      * @returns {boolean}
      */
     #isContentOverflowing() {
-        return this.overflowElement.scrollWidth > this.overflowElement.clientWidth;
+        return this.options.overflowElement.scrollWidth > this.options.overflowElement.clientWidth;
     }
 
     #appendZoomButton() {
         const zoomButton = document.createElement('button');
 
         Object.assign(zoomButton, {
-            textContent: this.buttonLabel,
+            textContent: this.options.buttonLabel,
             ariaHidden: 'true',
         });
 
-        zoomButton.classList.add('content-zoom-trigger');
+        zoomButton.classList.add(this.options.classes.triggerButton);
         zoomButton.addEventListener('click', this.toggleZoom);
 
-        this.container.prepend(zoomButton);
+        this.options.container.prepend(zoomButton);
     }
 
     toggleZoom = () => {
-        this.container.classList.toggle('zoom');
+        this.options.container.classList.toggle(this.options.classes.contendZoomed);
     };
 }
