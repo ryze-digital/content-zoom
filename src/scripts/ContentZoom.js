cd7798d5569d5bac94552afc6f1190284a8f566b
Christopher Canisius <cc@electronic-minds.de>
2025-10-17 18:38:35 +0200

    feat: Add button that toggles content zoom

--- src/scripts/ContentZoom.js
+++ src/scripts/ContentZoom.js
@@ -1,3 +1,49 @@
 export class ContentZoom {
-    constructor() { }
+    /**
+     * @param {HTMLElement} container
+     * @param {HTMLElement} overflowElement
+     * @param {string} buttonLabel
+     */
+    constructor(
+        container = document.querySelector('.content-zoom'),
+        overflowElement,
+        buttonLabel = 'Zoom'
+    ) {
+        if (typeof overflowElement === 'undefined') {
+            overflowElement = container;
+        }
+
+        this.container = container;
+        this.overflowElement = overflowElement;
+        this.buttonLabel = buttonLabel;
+
+        if (this.#isContentOverflowing) {
+            this.#appendZoomButton();
+        }
+    }
+
+    /**
+     * @returns {boolean}
+     */
+    #isContentOverflowing() {
+        return this.overflowElement.scrollWidth > this.overflowElement.clientWidth;
+    }
+
+    #appendZoomButton() {
+        const zoomButton = document.createElement('button');
+
+        Object.assign(zoomButton, {
+            textContent: this.buttonLabel,
+            ariaHidden: 'true',
+        });
+
+        zoomButton.classList.add('content-zoom-trigger');
+        zoomButton.addEventListener('click', this.toggleZoom);
+
+        this.container.prepend(zoomButton);
+    }
+
+    toggleZoom = () => {
+        this.container.classList.toggle('zoom');
+    };
 }
