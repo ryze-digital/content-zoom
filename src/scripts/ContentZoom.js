7ede6ceffac1852294b8f0498cabc47094e7aa23
Christopher Canisius <cc@electronic-minds.de>
2025-10-20 17:17:26 +0200

    feat: Hide button if there is not benefit of zooming

--- src/scripts/ContentZoom.js
+++ src/scripts/ContentZoom.js
@@ -1,16 +1,30 @@
-import { Base } from '@ryze-digital/js-utilities';
+import { Base, ReduceFunctionCalls } from '@ryze-digital/js-utilities';
 
 export class ContentZoom extends Base {
     /**
+     * @type {HTMLButtonElement}
+     * */
+    #zoomButton = document.createElement('button');
+
+    /**
+     * @type {boolean}
+     */
+    #contentZoomed = false;
+
+    /**
      * @param {object} options
-     * @param {HTMLElement} [options.container]
+     * @param {HTMLElement} [options.el]
+     * @param {boolean} [options.autoDetectOverflow]
      * @param {HTMLElement} [options.overflowElement]
      * @param {string} [options.buttonLabel]
      */
     constructor(options) {
         super({
-            container: document.querySelector('.content-zoom'),
+            el: document.querySelector('.content-zoom'),
+            autoDetectOverflow: true,
+            autoDetectZoomability: true,
             overflowElement: null,
+            zoomabilityElement: document.body,
             buttonLabel: 'Zoom',
             classes: {
                 contendZoomed: 'zoom',
@@ -19,36 +33,53 @@ export class ContentZoom extends Base {
         }, options);
 
         if (this.options.overflowElement === null) {
-            this.options.overflowElement = this.options.container;
+            this.options.overflowElement = this.options.el;
         }
 
-        if (this.#isContentOverflowing()) {
-            this.#appendZoomButton();
-        }
+        this.#appendZoomButton();
+        window.addEventListener('resize', ReduceFunctionCalls.throttle(this.#updateZoomButtonVisibility));
     }
 
     /**
      * @returns {boolean}
      */
     #isContentOverflowing() {
+        if (this.options.autoDetectOverflow === false) {
+            return true;
+        }
+
         return this.options.overflowElement.scrollWidth > this.options.overflowElement.clientWidth;
     }
 
-    #appendZoomButton() {
-        const zoomButton = document.createElement('button');
+    /**
+     * @returns {boolean}
+     */
+    #isViewportIsBiggerThanEl() {
+        if (this.options.autoDetectZoomability === false) {
+            return true;
+        }
 
-        Object.assign(zoomButton, {
+        return this.options.zoomabilityElement.clientWidth > this.options.el.clientWidth;
+    }
+
+    #appendZoomButton() {
+        Object.assign(this.#zoomButton, {
             textContent: this.options.buttonLabel,
-            ariaHidden: 'true',
+            ariaHidden: 'true'
         });
 
-        zoomButton.classList.add(this.options.classes.triggerButton);
-        zoomButton.addEventListener('click', this.toggleZoom);
+        this.#updateZoomButtonVisibility();
+        this.#zoomButton.classList.add(this.options.classes.triggerButton);
+        this.#zoomButton.addEventListener('click', this.toggleZoom);
 
-        this.options.container.prepend(zoomButton);
+        this.options.el.prepend(this.#zoomButton);
+    }
+
+    #updateZoomButtonVisibility = () => {
+        this.#zoomButton.hidden = !((this.#isContentOverflowing() && this.#isViewportIsBiggerThanEl()) || this.#contentZoomed === true);
     }
 
     toggleZoom = () => {
-        this.options.container.classList.toggle(this.options.classes.contendZoomed);
+        this.options.el.classList.toggle(this.options.classes.contendZoomed);
     };
 }
